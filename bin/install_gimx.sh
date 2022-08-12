1baee8a56539e6587714d73284756c72747689d1
bwxzgeretou <bwxzgeretou@gmail.com>
2022-08-12 11:06:09 +0900

    update GIMX

--- bin/install_gimx.sh
+++ bin/install_gimx.sh
@@ -1,4 +1,4 @@
 sudo apt install gdebi 
 sudo apt install libbluetooth3 libmhash2 libwxbase3.0-0v5 libwxgtk3.0-0v5 xterm avrdude
-wget http://gimx.fr/download/gimx-raspbian -O gimx.deb
+wget https://github.com/matlo/GIMX/releases/download/v8.0/gimx_8.0-1_armhf.deb -O gimx.deb
 sudo dpkg --force-overwrite -i gimx.deb
